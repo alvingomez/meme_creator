4fd939e7412758fa0a51131cfdfdcd5c28f095b7
Alvin Gomez <alvingomez.world@gmail.com>
2020-02-03 20:14:25 +0800

    I added the package.json and babel src. Installed the babel modules.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+ mode: 'development',
+ entry: {
+  app: './src/js/memes.js',
+  general: './src/js/general.js'
+ },
+ output: {
+  path: path.join(__dirname, 'public'),
+  filename: '[name].js'
+ },
+ module: {
+  rules: [{
+   loader: 'babel-loader',
+   test: /\.jss$/,
+   exclude: /node_modules/
+  },
+  {
+   test:/\.css$/,
+   use: [
+    {
+     loader: 'style-loader'
+    },
+    {
+     loader:'css-loader',
+     
+    }
+   ]
+  }
+ ]
+ }
+}
