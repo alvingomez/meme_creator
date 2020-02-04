7c7f7b82f97f23b1ed6a4a97479c1634e9abd49e
Alvin Gomez <alvingomez.world@gmail.com>
2020-02-04 12:27:22 +0800

    DOM reference variables
    Creating a reference to all the required DOM elements in the class. I used to the "$" to identify variables that contains a reference to the DOM.

--- src/js/memes.js
+++ src/js/memes.js
@@ -1,1 +1,14 @@
 import './general';
+
+
+class Memes {
+ constructor() {
+//Creating a reference to all the required DOM elements in the class.
+//Variables that start with a $ contain a reference to the DOM elements
+this.$canvas = document.querySelector('#imgCanvas');
+this.$topTextInput = document.querySelector('#topText');
+this.$bottomTextInput = document.querySelector('#bottomText');
+this.$imageInput  = document.querySelector('#image');
+this.$downloadButton = document.querySelector('#donwloadMeme');
+ }
+}
