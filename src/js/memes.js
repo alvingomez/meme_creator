b662b9693f2095bbbe7ba833f7ba36062ab8d332
Alvin Gomez <alvingomez.world@gmail.com>
2020-02-10 14:49:58 +0800

    Render funtion
    Created the Meme() function that will render the meme to the canvas. This will also contain the primary canvas renderer.

--- src/js/memes.js
+++ src/js/memes.js
@@ -3,29 +3,32 @@ import './general';
 //Variable to store device width
 const deviceWidth = window.innerWidth;
 
-
 class Memes {
- constructor() {
-//Creating a reference to all the required DOM elements in the class.
-//Variables that start with a $ contain a reference to the DOM elements
-this.$canvas = document.querySelector('#imgCanvas');
-this.$topTextInput = document.querySelector('#topText');
-this.$bottomTextInput = document.querySelector('#bottomText');
-this.$imageInput  = document.querySelector('#image');
-this.$downloadButton = document.querySelector('#donwloadMeme');
+   constructor() {
+   //Creating a reference to all the required DOM elements in the class.
+   //Variables that start with a $ contain a reference to the DOM elements
+      this.$canvas = document.querySelector('#imgCanvas');
+      this.$topTextInput = document.querySelector('#topText');
+      this.$bottomTextInput = document.querySelector('#bottomText');
+      this.$imageInput  = document.querySelector('#image');
+      this.$downloadButton = document.querySelector('#donwloadMeme');
 
-this.createCanvas;
- }
+      this.createCanvas;
+   }
 
- //Creating a Canvas with height and width 
- //Reduced the width by 30 to have space for the margins.
- createCanvas() {
-  let canvasHeight = Math.min(480, deviceWidth - 30);
-  let canvasWidth = Math.min(640, deviceWidth - 30);
-  this.$canvas.height = canvasHeight;
-  this.$canvas.width = canvasWidth; 
- 
-}
+    //Creating a Canvas with height and width 
+    //Reduced the width by 30 to have space for the margins.
+   createCanvas() {
+     let canvasHeight = Math.min(480, deviceWidth - 30);
+     let canvasWidth = Math.min(640, deviceWidth - 30);
+     this.$canvas.height = canvasHeight;
+     this.$canvas.width = canvasWidth;  
+   }
+   //A function that renders the meme to the canvas
+   //this will contain the primary canvas renderer
+   createMeme() {
+    console.log('rendered');
+   }
 }
 
 
